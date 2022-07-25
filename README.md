4429017ac74f74c4337fca8e86da456e608975fa
ittp <tp@ittps.ru>
2022-07-25 16:54:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-prfuia
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-prfuia)
---
+++ data/api.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const swe = require('swr');
+
+console.log(swr);
+const columns = {
+  id: 'ID',
+  key: 'УНО',
+  type: 'Тип',
+  manufacturer: 'Производитель',
+  model: 'Модель',
+  serial: 'SN',
+  inventory: 'Инв. номер',
+  year: 'Год',
+  place: 'Место',
+};
+
+const axios = require('axios');
